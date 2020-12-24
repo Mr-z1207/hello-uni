b6403ba252916c9355d97093cab83ead67f5a5c0
Mr-1207 <1436005686@qq.com>
2020-12-24 19:34:03 +0800

    test

---
+++ pages/toDoList/toDoList.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="todolist">
+		<view>
+			<text>文字</text>
+		</view>
+		<view>
+			<text selectable>可选文字</text>
+		</view>
+		<view>
+			<text space="emsp">文字    空格</text>
+		</view>
+		
+		<view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
